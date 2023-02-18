b779e13b9cc09dda4116793209d56ba1aa4f4f6d
Alex <monsta.epso@gmail.com>
2023-02-18 15:45:52 +0300

    modifity

--- Ya.py
+++ Ya.py
@@ -1,22 +1,33 @@
 import requests
+from pprint import pprint
+
 
 class YaUploader:
     def __init__(self, token: str):
         self.token = token
-        self.headers = {
-            'content-type': 'application/json',
-            'authorization': f'OAuth {self.token}'
+
+    def header(self):    
+        return{
+            'Content-type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
         }
 
-    def upload(self, file_path: str):
-        href = self._href(file_path)
-        requests.put(href)
-
-    def _href(self, file_path):
-        url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-        headers = self.headers
+    def _upload_link(self, file_path):
+        get_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.header()
         params = {'path': file_path, 'overwrite': 'true'}
-        response = requests.get(url, headers=headers, params=params).json()
-        href = response['href']
-        return href
+        response = requests.get(get_url, headers=headers, params=params)
+        pprint(response.json())
+        return response.json()
+    
+    def upload(self, file_path, filename):
+        href = self._upload_link(file_path=file_path).get("href", "")
+        response = requests.put(href, data=open(filename, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("Ok")
+
+
+
+
 
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from Ya import YaUploader
 TOKEN = ''
 
 if __name__ == '__main__':
-    path_to_file = 'readme.md'
+    
     token = TOKEN
     uploader = YaUploader(token)
-    uploader.upload(path_to_file)
+    uploader.upload( "readme.md", "readme.md")
